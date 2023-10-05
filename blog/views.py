7e25451033c97e9e8d26eb2e40a8eab9c1e84bc5
RGB00255 <ryan.bergeron1998@gmail.com>
2023-10-05 14:38:24 -0400

    End of chapter, ready for deployment.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 # blog/views.py
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = "post_detail.html"
