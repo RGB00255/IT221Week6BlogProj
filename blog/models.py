6422712ee85d7bb424cc1a236dc44cc99b664fa7
RGB00255 <ryan.bergeron1998@gmail.com>
2023-10-12 14:36:12 -0400

    Added login page.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.urls import reverse
 
 class Post(models.Model):
+
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         "auth.User",
